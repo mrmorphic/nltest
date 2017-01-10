f2ce05d58c993195f8b277c0fd32c9e011560b21
Mark Stephens <mr.morphic@gmail.com>
2017-01-10 15:17:51 +1300

    update readme

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MaterialDesignLightLayoutManager } from './materialdesignlayoutmanager';
+import { ComponentNode } from './componentnode.interface';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'layout-editor',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+  	<div class="layout-editor">
+  	  <h1>Layout Editor</h1>
+	  <nlcomponent [data]="getComponentData()" [layoutManager]="layoutManager"></nlcomponent>
+ 	</div>
+  `,
+  styles: [`
+  	.layout-editor {
+  	  padding: 3px;
+  	  margin: 5px;
+  	  background-color: #eee;
+  	}
+  `]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+	name = 'Page';
+
+	layoutManager = new MaterialDesignLightLayoutManager();
+
+	getComponentData(): ComponentNode {
+		var data = JSON.parse(`
+			{
+				"TypeName":"verticalBox",
+				"Children":[
+					{
+						"TypeName":"horizontalBox",
+						"Children":[
+							{
+								"TypeName":"plainText",
+								"Children":[],
+								"BindingDefs":{
+									"text":{
+										"Property":"text",
+										"Kind":0,
+										"Value":"hello"
+									}
+								},
+								"LayoutDefs":{
+									"materialDesignLite": {
+										"columns": {
+											"sm": {
+												"width": "6"
+											}
+										}
+									}
+								}
+							},
+							{
+								"TypeName":"plainText",
+								"Children":[],
+								"BindingDefs":{
+									"text":{"Property":"text","Kind":1,"Value":"foo"}
+								},
+								"LayoutDefs":{
+									"materialDesignLite": {
+										"columns": {
+											"sm": {
+												"width": "6"
+											}
+										}
+									}
+								}
+							}
+						],
+						"BindingDefs":{},
+						"LayoutDefs": {}
+					}
+				],
+				"BindingDefs":{},
+				"LayoutDefs":{}
+			}
+		`);
+		return data;
+	}
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ComponentComponent } from './component.component';
+import { VerticalBoxComponent } from './verticalbox.component';
+import { HorizontalBoxComponent } from './horizontalbox.component';
+import { GenericComponent } from './generic.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule
+  ],
+  declarations: [
+  	AppComponent,
+  	ComponentComponent,
+  	VerticalBoxComponent,
+  	HorizontalBoxComponent,
+  	GenericComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
